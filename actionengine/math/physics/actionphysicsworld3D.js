41d32f3af23465feb52ad11fb5a7f2a19b675632
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-12 15:29:16 -0700

    Add support for objects with multiple rigidbody and constraints

--- actionengine/math/physics/actionphysicsworld3D.js
+++ actionengine/math/physics/actionphysicsworld3D.js
@@ -1,51 +1,49 @@
 // actionengine/math/physics/actionphysicsworld3D.js
 class ActionPhysicsWorld3D {
-    constructor(fixedTimestep = 1/60) {
+    constructor(fixedTimestep = 1 / 60) {
         const broadphase = new Goblin.SAPBroadphase();
         const narrowphase = new Goblin.NarrowPhase();
         const solver = new Goblin.IterativeSolver();
         this.world = new Goblin.World(broadphase, narrowphase, solver);
-        
+
         this.world.gravity = new Goblin.Vector3(0, -98.1, 0);
-        
+
         this.objects = new Set();
-        this.constraints = new Set();
-        
+
         // Physics timing variables
         this.fixedTimeStep = fixedTimestep;
         this.physicsAccumulator = 0;
         this.lastPhysicsTime = performance.now();
         this.isPaused = false;
     }
-    
+
     update(deltaTime) {
         if (!this.world || this.isPaused) return;
-        
+
         const currentTime = performance.now();
         const frameTime = Math.min((currentTime - this.lastPhysicsTime) / 1000, 0.25); // Cap at 250ms
         this.lastPhysicsTime = currentTime;
         this.physicsAccumulator += frameTime;
-        
+
         while (this.physicsAccumulator >= this.fixedTimeStep) {
             this.world.step(this.fixedTimeStep);
             this.physicsAccumulator -= this.fixedTimeStep;
-            this.objects.forEach(object => {
+            this.objects.forEach((object) => {
                 if (object.body) {
                     object.updateVisual();
                 }
             });
         }
     }
-    
+
     addConstraint(constraint) {
-            if (!constraint) {
-                console.warn("[PhysicsWorld] Attempted to add null constraint");
-                return;
-            }
-            //console.log("[PhysicsWorld] Adding constraint:", constraint);
-            this.constraints.add(constraint);
-            this.world.addConstraint(constraint);
+        if (!constraint) {
+            console.warn("[PhysicsWorld] Attempted to add null constraint");
+            return;
         }
+        //console.log("[PhysicsWorld] Adding constraint:", constraint);
+        this.world.addConstraint(constraint);
+    }
 
     addObject(object) {
         this.objects.add(object);
@@ -53,10 +51,22 @@ class ActionPhysicsWorld3D {
             //console.log("[PhysicsWorld] Adding object body:", object.body);
             this.world.addRigidBody(object.body);
         }
+        if (object.rigidBodies) {
+            object.rigidBodies.forEach((body) => {
+                console.log("[PhysicsWorld] Adding object body:", body);
+                this.world.addRigidBody(body);
+            });
+        }
+        if (object.constraints) {
+            object.constraints.forEach((constraint) => {
+                console.log("[PhysicsWorld] Adding object constraint:", constraint);
+                this.world.addConstraint(constraint);
+            });
+        }
     }
 
     addRigidBody(body, group = 0, mask = 0) {
-        /*
+        /* //disable masking for now
         if (group !== undefined || mask !== undefined) {
             body.collision_groups = group || 1;
             body.collision_mask = mask || -1;
@@ -66,17 +76,17 @@ class ActionPhysicsWorld3D {
     }
 
     addTerrainBody(body, group = 0, mask = 0) {
-        
         //console.log("[PhysicsWorld] Adding terrain body:", body);
         if (this.terrainBody) {
             //console.log("[PhysicsWorld] Removing old terrain body");
             this.world.removeRigidBody(this.terrainBody);
         }
         this.terrainBody = body;
-
+        
+        //disable masking for now
         //body.collision_groups = group;
         //body.collision_mask = mask;
-
+        
         //console.log("[PhysicsWorld] Adding to world with groups:", group, "mask:", mask);
         this.world.addRigidBody(body);
     }
@@ -84,21 +94,33 @@ class ActionPhysicsWorld3D {
     removeRigidBody(body) {
         this.world.removeRigidBody(body);
     }
-    
+
     removeConstraint(constraint) {
         if (!constraint) {
-            //console.warn("[PhysicsWorld] Attempted to remove null constraint");
+            console.warn("[PhysicsWorld] Attempted to remove null constraint");
             return;
         }
-        this.constraints.delete(constraint);
         this.world.removeConstraint(constraint);
     }
-    
+
     removeObject(object) {
-        this.objects.delete(object);
         if (object.body) {
             this.world.removeRigidBody(object.body);
         }
+        // Check for additional bodies
+        if (object.rigidBodies) {
+            object.rigidBodies.forEach((body) => {
+                //console.log("[PhysicsWorld] Removing object body:", body);
+                this.world.removeRigidBody(body);
+            });
+        }
+        if (object.constraints) {
+            object.constraints.forEach((constraint) => {
+                //console.log("[PhysicsWorld] Removing object constraint:", constraint);
+                this.world.removeConstraint(constraint);
+            });
+        }
+        this.objects.delete(object);
     }
 
     pause() {
@@ -110,32 +132,24 @@ class ActionPhysicsWorld3D {
         this.lastPhysicsTime = performance.now();
         this.physicsAccumulator = 0;
     }
-    
+
     reset() {
         if (this.terrainBody) {
             this.world.removeRigidBody(this.terrainBody);
             this.terrainBody = null;
         }
-        
-        this.objects.forEach(obj => {
-            if (obj.body) {
-                this.world.removeRigidBody(obj.body);
-            }
+
+        this.objects.forEach((obj) => {
+            this.removeObject(obj);
         });
         
-        // Clear all constraints
-        this.constraints.forEach(constraint => {
-            this.world.removeConstraint(constraint);
-        });
-        
-        this.constraints.clear();
         this.objects.clear();
     }
 
     setShaderManager(shaderManager) {
         this.shaderManager = shaderManager;
     }
-    
+
     getWorld() {
         return this.world;
     }
