819e4959e89470918b9483893d1a068a670778d2
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-12 15:28:31 -0700

    minor cleanup

--- actionengine/math/physics/actionphysicsobject3D.js
+++ actionengine/math/physics/actionphysicsobject3D.js
@@ -24,8 +24,6 @@ class ActionPhysicsObject3D extends RenderableObject {
                 ));
             });
         });
-        
-        //this.physicsWorld.addObject(this);
     }
 
     updateVisual() {
