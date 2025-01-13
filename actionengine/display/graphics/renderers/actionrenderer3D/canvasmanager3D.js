9f0db1025bb827a82f4b193b218f40f4a975261d
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-13 01:12:33 -0700

    Fix typo and clean

--- actionengine/display/graphics/renderers/actionrenderer3D/canvasmanager3D.js
+++ actionengine/display/graphics/renderers/actionrenderer3D/canvasmanager3D.js
@@ -1,11 +1,11 @@
-// actionengine/display/graphics/renderers/actionrenderer3D/weatherrenderer3D.js
+// actionengine/display/graphics/renderers/actionrenderer3D/canvasmanager3D.js
 class CanvasManager3D {
     constructor(canvas) {
         this.canvas = canvas;
         this.initializeContext();
         this.initializeGLState();
     }
-
+    
     initializeContext() {
         this.gl = this.canvas.getContext("webgl2");
         if (!this.gl) {
@@ -13,61 +13,61 @@ class CanvasManager3D {
             if (!this.gl) {
                 throw new Error("WebGL not supported");
             }
-            this._isWebGL2 = false;  // Changed to _isWebGL2
+            this._isWebGL2 = false;
             console.log("[CanvasManager3D] Using WebGL 1.0");
         } else {
-            this._isWebGL2 = true;   // Changed to _isWebGL2
+            this._isWebGL2 = true;
             console.log("[CanvasManager3D] Using WebGL 2.0");
         }
     }
-
+    
     initializeGLState() {
         // Basic setup
         this.gl.clearColor(0.529, 0.808, 0.922, 1.0);
         this.gl.viewport(0, 0, Game.WIDTH, Game.HEIGHT);
-
+        
         // Depth testing setup
         this.gl.enable(this.gl.DEPTH_TEST);
         this.gl.depthFunc(this.gl.LEQUAL);
-
+        
         // Face culling setup
         this.gl.enable(this.gl.CULL_FACE);
         this.gl.frontFace(this.gl.CCW);
         this.gl.cullFace(this.gl.BACK);
-
+        
         // Disable blending by default
         this.gl.disable(this.gl.BLEND);
     }
-
+    
     getContext() {
         return this.gl;
     }
-
+    
     isWebGL2() {
-        return this._isWebGL2;  // Return the private property
+        return this._isWebGL2;
     }
-
+    
     clear() {
         this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
     }
-
+    
     setViewport(width, height) {
         this.gl.viewport(0, 0, width, height);
     }
-
+    
     enableBlending() {
         this.gl.enable(this.gl.BLEND);
         this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
     }
-
+    
     disableBlending() {
         this.gl.disable(this.gl.BLEND);
     }
-
+    
     bindFramebuffer(framebuffer) {
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, framebuffer);
     }
-
+    
     resetToDefaultFramebuffer() {
         this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
         this.setViewport(Game.WIDTH, Game.HEIGHT);
