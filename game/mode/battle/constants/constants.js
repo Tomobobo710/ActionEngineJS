9745c6cf6ed7c74da8458a472ecff939c8ab2665
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-13 18:33:57 -0700

    Make a more generic constants script

---
+++ game/mode/battle/constants/constants.js
@@ -0,0 +1,83 @@
+// game/mode/battle/constants/constants.js
+const TARGET_TYPES = {
+    SINGLE_ALLY: "single_ally",
+    ALL_ALLIES: "all_allies",
+    SINGLE_ENEMY: "single_enemy",
+    ALL_ENEMIES: "all_enemies"
+};
+
+const DEFAULT_PARTY = [
+    {
+        name: "Cecil",
+        type: "warrior",
+        level: 10,
+        maxHp: 150,
+        maxMp: 30,
+        strength: 15,
+        magic: 8,
+        speed: 12,
+        skills: ["Attack", "Defend"],
+        spells: ["fire"]
+    },
+    {
+        name: "Rosa",
+        type: "mage",
+        level: 10,
+        maxHp: 90,
+        maxMp: 80,
+        strength: 6,
+        magic: 18,
+        speed: 10,
+        skills: ["Attack"],
+        spells: ["fire", "ice", "lightning", "poison", "heal", "quake", "wind", "water", "holy"]
+    },
+    {
+        name: "Edge",
+        type: "thief",
+        level: 10,
+        maxHp: 110,
+        maxMp: 45,
+        strength: 13,
+        magic: 10,
+        speed: 16,
+        skills: ["Attack", "Steal"],
+        spells: ["lightning", "poison"]
+    }
+];
+
+const ENEMY_TEMPLATES = {
+    slime: {
+        type: "slime",
+        maxHp: 30,
+        maxMp: 20,
+        strength: 8,
+        magic: 5,
+        speed: 12,
+        spells: ["poison"]
+    },
+    bat: {
+        type: "bat",
+        maxHp: 45,
+        maxMp: 35,
+        strength: 12,
+        magic: 8,
+        speed: 14,
+        spells: ["wind"]
+    },
+    goblin: {
+        type: "goblin",
+        maxHp: 65,
+        maxMp: 25,
+        strength: 14,
+        magic: 6,
+        speed: 16,
+        spells: ["fire"]
+    }
+};
+
+const STARTER_INVENTORY = [
+    { item: "potion", quantity: 5 },
+    { item: "megaPotion", quantity: 2 },
+    { item: "poison", quantity: 3 },
+    { item: "bomb", quantity: 2 }
+];
--- game/mode/battle/constants/targettypes.js
+++
@@ -1,7 +0,0 @@
-// game/mode/battle/constants/targettypes.js
-const TARGET_TYPES = {
-    SINGLE_ALLY: "single_ally",
-    ALL_ALLIES: "all_allies",
-    SINGLE_ENEMY: "single_enemy",
-    ALL_ENEMIES: "all_enemies"
-};
