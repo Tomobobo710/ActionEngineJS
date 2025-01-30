60cdf5dfb9f8b23a785af3b730b765cfd89172ca
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-30 15:33:51 -0700

    Add texture support to terrain shader

--- game/display/gl/shaders/defaultshader.js
+++ game/display/gl/shaders/defaultshader.js
@@ -2,34 +2,49 @@
 class DefaultShader {
     getTerrainVertexShader(isWebGL2) {
         return `${isWebGL2 ? "#version 300 es\n" : ""}
-        ${isWebGL2 ? "in" : "attribute"} vec3 aPosition;
-        ${isWebGL2 ? "in" : "attribute"} vec3 aNormal;
-        ${isWebGL2 ? "in" : "attribute"} vec3 aColor;
+    ${isWebGL2 ? "in" : "attribute"} vec3 aPosition;
+    ${isWebGL2 ? "in" : "attribute"} vec3 aNormal;
+    ${isWebGL2 ? "in" : "attribute"} vec3 aColor;
+    ${isWebGL2 ? "in" : "attribute"} vec2 aTexCoord;  // Add this
+    
+    uniform mat4 uProjectionMatrix;
+    uniform mat4 uViewMatrix;
+    uniform mat4 uModelMatrix;
+    uniform vec3 uLightDir;
+    uniform bool uUseTexture;
+    
+    ${isWebGL2 ? "flat out vec3 vColor;" : "varying vec3 vColor;"}
+    ${isWebGL2 ? "out" : "varying"} vec2 vTexCoord;
+    ${isWebGL2 ? "out" : "varying"} float vLighting;
+    
+    void main() {
+        gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
         
-        uniform mat4 uProjectionMatrix;
-        uniform mat4 uViewMatrix;
-        uniform mat4 uModelMatrix;
-        uniform vec3 uLightDir;
+        vec3 worldNormal = normalize(mat3(uModelMatrix) * aNormal);
+        vLighting = max(0.3, min(1.0, dot(worldNormal, normalize(uLightDir))));
+        vColor = aColor;
         
-        ${isWebGL2 ? "flat out vec3 vColor;" : "varying vec3 vColor;"}
-        
-        void main() {
-            gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPosition, 1.0);
-            
-            vec3 worldNormal = normalize(mat3(uModelMatrix) * aNormal);
-            float lighting = max(0.3, min(1.0, dot(worldNormal, normalize(uLightDir))));
-            vColor = aColor * lighting;
-        }`;
+        vTexCoord = uUseTexture ? aTexCoord : vec2(0.0, 0.0);
+    }`;
     }
 
     getTerrainFragmentShader(isWebGL2) {
         return `${isWebGL2 ? "#version 300 es\n" : ""}
-        precision mediump float;
-        ${isWebGL2 ? "flat in vec3 vColor;\nout vec4 fragColor;" : "varying vec3 vColor;"}
-        
-        void main() {
-            ${isWebGL2 ? "fragColor" : "gl_FragColor"} = vec4(vColor, 1.0);
-        }`;
+    precision mediump float;
+    ${isWebGL2 ? "flat in vec3 vColor;\nout vec4 fragColor;" : "varying vec3 vColor;"}
+    ${isWebGL2 ? "in" : "varying"} vec2 vTexCoord;
+    ${isWebGL2 ? "in" : "varying"} float vLighting;
+    uniform sampler2D uTexture;
+    uniform bool uUseTexture;
+    
+    void main() {
+        if (uUseTexture) {
+            vec4 texColor = ${isWebGL2 ? "texture" : "texture2D"}(uTexture, vTexCoord);
+            ${isWebGL2 ? "fragColor" : "gl_FragColor"} = texColor * vec4(vec3(vLighting), 1.0);
+        } else {
+            ${isWebGL2 ? "fragColor" : "gl_FragColor"} = vec4(vColor * vLighting, 1.0);
+        }
+    }`;
     }
 
     getCharacterVertexShader() {
@@ -98,22 +113,22 @@ class DefaultShader {
     }
 
     getLineVertexShader() {
-    return `${this.isWebGL2 ? "#version 300 es\n" : ""}
+        return `${this.isWebGL2 ? "#version 300 es\n" : ""}
     ${this.isWebGL2 ? "in" : "attribute"} vec3 aPosition;
     uniform mat4 uProjectionMatrix;
     uniform mat4 uViewMatrix;
     void main() {
         gl_Position = uProjectionMatrix * uViewMatrix * vec4(aPosition, 1.0);
     }`;
-}
+    }
 
-getLineFragmentShader() {
-    return `${this.isWebGL2 ? "#version 300 es\n" : ""}
+    getLineFragmentShader() {
+        return `${this.isWebGL2 ? "#version 300 es\n" : ""}
     precision mediump float;
     uniform vec3 uColor;
     ${this.isWebGL2 ? "out vec4 fragColor;\n" : ""}
     void main() {
         ${this.isWebGL2 ? "fragColor" : "gl_FragColor"} = vec4(uColor, 1.0);
     }`;
-}
+    }
 }
