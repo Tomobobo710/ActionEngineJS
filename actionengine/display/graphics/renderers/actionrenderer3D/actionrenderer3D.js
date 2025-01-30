19539ebd3da21d60e59c3521860622cbd31b8cd6
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-30 15:30:53 -0700

    Add TextureManager addon class
    Temporarily stored in ActionRenderer3D for now

--- actionengine/display/graphics/renderers/actionrenderer3D/actionrenderer3D.js
+++ actionengine/display/graphics/renderers/actionrenderer3D/actionrenderer3D.js
@@ -17,8 +17,8 @@ class ActionRenderer3D {
         this.objectRenderer = new ObjectRenderer3D(this, this.gl, this.programManager, this.lightingManager);
         // Get program registry reference
         this.programRegistry = this.programManager.getProgramRegistry();
-		this.waterRenderer = new WaterRenderer3D(this.gl, this.programManager);
-
+        this.waterRenderer = new WaterRenderer3D(this.gl, this.programManager);
+        this.textureManager = new TextureManager(this.gl);
         // Time tracking
         this.startTime = performance.now();
         this.currentTime = 0;
@@ -45,13 +45,13 @@ class ActionRenderer3D {
 
         // Get current shader set
         const shaderSet = this.programRegistry.getCurrentShaderSet();
-        
+
         if (shaderSet === this.programRegistry.shaders.get("virtualboy")) {
-           this.canvasManager.setClearColor(0.0, 0.0, 0.0, 1.0); // Black
+            this.canvasManager.setClearColor(0.0, 0.0, 0.0, 1.0); // Black
         } else {
-           this.canvasManager.setClearColor(0.529, 0.808, 0.922, 1.0); // Original blue
+            this.canvasManager.setClearColor(0.529, 0.808, 0.922, 1.0); // Original blue
         }
-        
+
         // SHADOW PASS
         if (shaderSet === this.programRegistry.shaders.get("pbr")) {
             this.canvasManager.bindFramebuffer(this.lightingManager.getShadowFramebuffer());
@@ -108,17 +108,17 @@ class ActionRenderer3D {
                 this.currentTime
             );
         }
-		
+
         // Render objects if they exist
         if (renderableObjects?.length) {
-		   for (const object of renderableObjects) {
-			   if (object instanceof Ocean) {
-				   this.waterRenderer.render(renderData.camera, this.currentTime, object);
-			   } else if (object) {
-				   this.objectRenderer.render(object, renderData.camera, shaderSet, this.currentTime);
-			   }
-		   }
-		}
+            for (const object of renderableObjects) {
+                if (object instanceof Ocean) {
+                    this.waterRenderer.render(renderData.camera, this.currentTime, object);
+                } else if (object) {
+                    this.objectRenderer.render(object, renderData.camera, shaderSet, this.currentTime);
+                }
+            }
+        }
 
         // Render weather if it exists
         if (weatherSystem) {
@@ -132,5 +132,63 @@ class ActionRenderer3D {
             }
             this.debugRenderer.drawDebugShadowMap();
         }
+    }
+}
+
+class TextureManager {
+    constructor(gl) {
+        this.gl = gl;
+        this.textures = new Map();
+
+        // Create procedural textures
+        const grassTexture = new ProceduralTexture(256, 256);
+        grassTexture.generateGrass();
+        this.grassTexture = this.createTextureFromProcedural(grassTexture);
+
+        const checkerTexture = new ProceduralTexture(256, 256);
+        checkerTexture.generateCheckerboard();
+        this.checkerTexture = this.createTextureFromProcedural(checkerTexture);
+    }
+
+    createTextureFromProcedural(proceduralTexture) {
+        const texture = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+
+        // Convert the data to RGBA format explicitly
+        const rgbaData = new Uint8Array(proceduralTexture.width * proceduralTexture.height * 4);
+        for (let i = 0; i < proceduralTexture.data.length; i += 4) {
+            rgbaData[i] = proceduralTexture.data[i]; // R
+            rgbaData[i + 1] = proceduralTexture.data[i + 1]; // G
+            rgbaData[i + 2] = proceduralTexture.data[i + 2]; // B
+            rgbaData[i + 3] = 255; // A (fully opaque)
+        }
+
+        this.gl.texImage2D(
+            this.gl.TEXTURE_2D,
+            0, // mipmap level
+            this.gl.RGBA, // internal format
+            proceduralTexture.width,
+            proceduralTexture.height,
+            0, // border
+            this.gl.RGBA, // format
+            this.gl.UNSIGNED_BYTE, // type
+            rgbaData
+        );
+
+        // Setup texture parameters
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+
+        return texture;
+    }
+
+    getGrassTexture() {
+        return this.grassTexture;
+    }
+
+    getCheckerTexture() {
+        return this.checkerTexture;
     }
 }
