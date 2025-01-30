5ebf0af2afededc6286a2d53fca15a96a1e3115d
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-30 15:31:28 -0700

    Add texture uniforms

--- actionengine/display/graphics/renderers/actionrenderer3D/programmanager.js
+++ actionengine/display/graphics/renderers/actionrenderer3D/programmanager.js
@@ -8,11 +8,11 @@ class ProgramManager {
         // Store shader programs
         this.particleProgram = null;
         this.debugShadowMapProgram = null;
-        
+
         // Store locations for different shader programs
         this.particleLocations = {};
         this.debugShadowLocations = {};
-        
+
         // Debug visualization buffers
         this.debugQuadBuffer = null;
         this.debugBackgroundBuffer = null;
@@ -25,7 +25,7 @@ class ProgramManager {
     initializeShaderPrograms() {
         this.initializeParticleShader();
         this.initializeDebugShadowShader();
-		this.initializeWaterShader();
+        this.initializeWaterShader();
         this.initializeDefaultShaders();
     }
 
@@ -35,7 +35,7 @@ class ProgramManager {
             particleShader.getParticleVertexShader(this.isWebGL2),
             particleShader.getParticleFragmentShader(this.isWebGL2)
         );
-        
+
         this.particleLocations = {
             position: this.gl.getAttribLocation(this.particleProgram, "aPosition"),
             size: this.gl.getAttribLocation(this.particleProgram, "aSize"),
@@ -45,40 +45,40 @@ class ProgramManager {
         };
     }
 
-initializeWaterShader() {
-    const waterShader = new WaterShader();
-    this.waterProgram = this.programRegistry.createShaderProgram(
-        waterShader.getWaterVertexShader(this.isWebGL2),
-        waterShader.getWaterFragmentShader(this.isWebGL2)
-    );
-    
-    // Add null checks
-    if (!this.waterProgram) {
-        console.error("Failed to create water program");
-        return;
-    }
-    
-    this.waterLocations = {
-        position: this.gl.getAttribLocation(this.waterProgram, "aPosition"),
-        normal: this.gl.getAttribLocation(this.waterProgram, "aNormal"),
-        texCoord: this.gl.getAttribLocation(this.waterProgram, "aTexCoord"),
-        projectionMatrix: this.gl.getUniformLocation(this.waterProgram, "uProjectionMatrix"),
-        viewMatrix: this.gl.getUniformLocation(this.waterProgram, "uViewMatrix"),
-        modelMatrix: this.gl.getUniformLocation(this.waterProgram, "uModelMatrix"),
-        time: this.gl.getUniformLocation(this.waterProgram, "uTime"),
-        cameraPos: this.gl.getUniformLocation(this.waterProgram, "uCameraPos"),
-        lightDir: this.gl.getUniformLocation(this.waterProgram, "uLightDir")
-    };
+    initializeWaterShader() {
+        const waterShader = new WaterShader();
+        this.waterProgram = this.programRegistry.createShaderProgram(
+            waterShader.getWaterVertexShader(this.isWebGL2),
+            waterShader.getWaterFragmentShader(this.isWebGL2)
+        );
 
-    console.log("Water locations:", this.waterLocations); // Debug
-}
+        // Add null checks
+        if (!this.waterProgram) {
+            console.error("Failed to create water program");
+            return;
+        }
+
+        this.waterLocations = {
+            position: this.gl.getAttribLocation(this.waterProgram, "aPosition"),
+            normal: this.gl.getAttribLocation(this.waterProgram, "aNormal"),
+            texCoord: this.gl.getAttribLocation(this.waterProgram, "aTexCoord"),
+            projectionMatrix: this.gl.getUniformLocation(this.waterProgram, "uProjectionMatrix"),
+            viewMatrix: this.gl.getUniformLocation(this.waterProgram, "uViewMatrix"),
+            modelMatrix: this.gl.getUniformLocation(this.waterProgram, "uModelMatrix"),
+            time: this.gl.getUniformLocation(this.waterProgram, "uTime"),
+            cameraPos: this.gl.getUniformLocation(this.waterProgram, "uCameraPos"),
+            lightDir: this.gl.getUniformLocation(this.waterProgram, "uLightDir")
+        };
+
+        console.log("Water locations:", this.waterLocations); // Debug
+    }
     initializeDebugShadowShader() {
         const debugShadowMapShader = new DebugShadowMapShader();
         this.debugShadowMapProgram = this.programRegistry.createShaderProgram(
             debugShadowMapShader.getDebugShadowMapVertexShader(this.isWebGL2),
             debugShadowMapShader.getDebugShadowMapFragmentShader(this.isWebGL2)
         );
-        
+
         this.debugShadowLocations = {
             position: this.gl.getAttribLocation(this.debugShadowMapProgram, "aPosition"),
             shadowMap: this.gl.getUniformLocation(this.debugShadowMapProgram, "uShadowMap")
@@ -121,7 +121,10 @@ initializeWaterShader() {
                     viewMatrix: this.gl.getUniformLocation(defaultTerrainProgram, "uViewMatrix"),
                     modelMatrix: this.gl.getUniformLocation(defaultTerrainProgram, "uModelMatrix"),
                     cameraPos: this.gl.getUniformLocation(defaultTerrainProgram, "uCameraPos"),
-                    lightDir: this.gl.getUniformLocation(defaultTerrainProgram, "uLightDir")
+                    lightDir: this.gl.getUniformLocation(defaultTerrainProgram, "uLightDir"),
+                    texture: this.gl.getUniformLocation(defaultTerrainProgram, "uTexture"),
+                    texCoord: this.gl.getAttribLocation(defaultTerrainProgram, "aTexCoord"),
+                    useTexture: this.gl.getUniformLocation(defaultTerrainProgram, "uUseTexture")
                 }
             },
             character: {
@@ -158,20 +161,28 @@ initializeWaterShader() {
         // Create debug quad buffer for shadow map visualization
         this.debugQuadBuffer = this.gl.createBuffer();
         const quadVertices = new Float32Array([
-            -1.0, -1.0,  // bottom left
-            -0.5, -1.0,  // bottom right
-            -1.0, -0.5,  // top left
-            -0.5, -0.5   // top right
+            -1.0,
+            -1.0, // bottom left
+            -0.5,
+            -1.0, // bottom right
+            -1.0,
+            -0.5, // top left
+            -0.5,
+            -0.5 // top right
         ]);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.debugQuadBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, quadVertices, this.gl.STATIC_DRAW);
 
         this.debugBackgroundBuffer = this.gl.createBuffer();
         const backgroundQuad = new Float32Array([
-            -1.2, -1.2,  // noticeably larger
-            1.2, -1.2,
-            -1.2, 1.2,
-            1.2, 1.2
+            -1.2,
+            -1.2, // noticeably larger
+            1.2,
+            -1.2,
+            -1.2,
+            1.2,
+            1.2,
+            1.2
         ]);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.debugBackgroundBuffer);
         this.gl.bufferData(this.gl.ARRAY_BUFFER, backgroundQuad, this.gl.STATIC_DRAW);
@@ -186,13 +197,13 @@ initializeWaterShader() {
         return this.particleLocations;
     }
 
-getWaterProgram() {
-    return this.waterProgram;
-}
+    getWaterProgram() {
+        return this.waterProgram;
+    }
 
-getWaterLocations() {
-    return this.waterLocations;
-}
+    getWaterLocations() {
+        return this.waterLocations;
+    }
 
     getDebugShadowProgram() {
         return this.debugShadowMapProgram;
