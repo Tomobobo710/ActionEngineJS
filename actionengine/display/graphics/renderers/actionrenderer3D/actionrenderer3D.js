9bf97ee3beda654a621d81683d3a5cfa7ed07545
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-31 19:21:43 -0700

    Render textures per triangle

--- actionengine/display/graphics/renderers/actionrenderer3D/actionrenderer3D.js
+++ actionengine/display/graphics/renderers/actionrenderer3D/actionrenderer3D.js
@@ -138,57 +138,61 @@ class ActionRenderer3D {
 class TextureManager {
     constructor(gl) {
         this.gl = gl;
-        this.textures = new Map();
-
-        // Create procedural textures
-        const grassTexture = new ProceduralTexture(256, 256);
-        grassTexture.generateGrass();
-        this.grassTexture = this.createTextureFromProcedural(grassTexture);
-
-        const checkerTexture = new ProceduralTexture(256, 256);
-        checkerTexture.generateCheckerboard();
-        this.checkerTexture = this.createTextureFromProcedural(checkerTexture);
+        this.textureArray = this.createTextureArray();
     }
 
-    createTextureFromProcedural(proceduralTexture) {
-        const texture = this.gl.createTexture();
-        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+    createTextureArray() {
+        const array = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D_ARRAY, array);
 
-        // Convert the data to RGBA format explicitly
-        const rgbaData = new Uint8Array(proceduralTexture.width * proceduralTexture.height * 4);
-        for (let i = 0; i < proceduralTexture.data.length; i += 4) {
-            rgbaData[i] = proceduralTexture.data[i]; // R
-            rgbaData[i + 1] = proceduralTexture.data[i + 1]; // G
-            rgbaData[i + 2] = proceduralTexture.data[i + 2]; // B
-            rgbaData[i + 3] = 255; // A (fully opaque)
-        }
-
-        this.gl.texImage2D(
-            this.gl.TEXTURE_2D,
-            0, // mipmap level
+        // All our procedural textures are 256x256
+        this.gl.texImage3D(
+            this.gl.TEXTURE_2D_ARRAY,
+            0, // mip level
             this.gl.RGBA, // internal format
-            proceduralTexture.width,
-            proceduralTexture.height,
+            256, // width
+            256, // height
+            textureRegistry.getTextureCount(), // number of layers
             0, // border
             this.gl.RGBA, // format
             this.gl.UNSIGNED_BYTE, // type
-            rgbaData
+            null // data
         );
 
-        // Setup texture parameters
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+        // Load each texture as a layer
+        textureRegistry.textureList.forEach((textureName, i) => {
+            const proceduralTexture = textureRegistry.get(textureName);
 
-        return texture;
-    }
+            // Convert to RGBA format
+            const rgbaData = new Uint8Array(proceduralTexture.width * proceduralTexture.height * 4);
+            for (let j = 0; j < proceduralTexture.data.length; j += 4) {
+                rgbaData[j] = proceduralTexture.data[j]; // R
+                rgbaData[j + 1] = proceduralTexture.data[j + 1]; // G
+                rgbaData[j + 2] = proceduralTexture.data[j + 2]; // B
+                rgbaData[j + 3] = 255; // A
+            }
 
-    getGrassTexture() {
-        return this.grassTexture;
-    }
+            this.gl.texSubImage3D(
+                this.gl.TEXTURE_2D_ARRAY,
+                0, // mip level
+                0, // x offset
+                0, // y offset
+                i, // z offset (layer)
+                256, // width
+                256, // height
+                1, // depth
+                this.gl.RGBA,
+                this.gl.UNSIGNED_BYTE,
+                rgbaData
+            );
+        });
 
-    getCheckerTexture() {
-        return this.checkerTexture;
+        // Set texture parameters
+        this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+        this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
+        this.gl.texParameteri(this.gl.TEXTURE_2D_ARRAY, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+
+        return array;
     }
 }
