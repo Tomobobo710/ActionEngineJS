c51f004ad1333eab7e988188eafaf7b44eb6c579
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-01-31 19:19:02 -0700

    Add NULL biome type

--- game/world/worldgen/biometypes.js
+++ game/world/worldgen/biometypes.js
@@ -1,49 +1,54 @@
 // game/world/worldgen/biometypes.js
 // Global biome type definitions
 const BIOME_TYPES = {
+    NULL: {
+        heightRange: [-100, -1],
+        base: "#0a1525",
+        texture: null
+    },
     OCEAN_DEEP: {
         heightRange: [-100, -1],
         base: "#0a1525",
-        texture: 'deepwater'
+        texture: "deepwater"
     },
     OCEAN: {
         heightRange: [0, 0],
         base: "#1a3045",
-        texture: 'water'
+        texture: "water"
     },
     BEACH: {
         heightRange: [0, 1],
         base: "#d2b98b",
-        texture: 'sand'
+        texture: "sand"
     },
     DUNES: {
         heightRange: [1, 2],
         base: "#ffe599",
-        texture: 'dunes'
+        texture: "dunes"
     },
     LOWLAND: {
         heightRange: [2, 15],
         base: "#407339",
-        texture: 'grass'
+        texture: "grass"
     },
     HIGHLAND: {
         heightRange: [15, 40],
         base: "#2d5929",
-        texture: 'highland'
+        texture: "highland"
     },
     TREELINE: {
         heightRange: [40, 50],
         base: "#744700",
-        texture: 'treeline'
+        texture: "treeline"
     },
     MOUNTAIN: {
         heightRange: [50, 90],
         base: "#736d69",
-        texture: 'rock'
+        texture: "rock"
     },
     SNOW: {
         heightRange: [90, 100],
         base: "#e8e8e8",
-        texture: 'snow'  // Snow stays untextured
+        texture: "snow" // Snow stays untextured
     }
 };
